95fc3bc4bc69ae57c17dd275e1f305c7eff9e55b
Félix Alejandro Zelaya Orellana <fazozela@gmail.com>
2024-07-07 21:55:56 -0400

    Update movefiles.js

--- movefiles.js
+++ movefiles.js
@@ -1,33 +1,19 @@
-const fs = require('fs-extra');
+const fs = require('fs');
 const path = require('path');
+const fse = require('fs-extra');
 
-const sourceDir = path.join(__dirname, 'dist', 'selfdiscovered', 'browser'); // Source directory
-const targetDir = path.join(__dirname, 'dist', 'selfdiscovered'); // Target directory
+const sourceDir = path.join(__dirname, 'dist', 'selfdiscovered', 'browser');
+const destDir = path.join(__dirname, 'dist', 'selfdiscovered');
 
-// Function to move files from source to target directory
-async function moveFiles() {
-  try {
-    // Check if source directory exists
-    if (await fs.pathExists(sourceDir)) {
-      // Read files from source directory
-      const files = await fs.readdir(sourceDir);
+if (fs.existsSync(sourceDir)) {
+  fs.readdirSync(sourceDir).forEach(file => {
+    const sourceFile = path.join(sourceDir, file);
+    const destFile = path.join(destDir, file);
+    fse.moveSync(sourceFile, destFile, { overwrite: true });
+  });
 
-      // Move each file to target directory, overwrite if exists
-      for (const file of files) {
-        const sourceFile = path.join(sourceDir, file);
-        const targetFile = path.join(targetDir, file);
-        await fs.move(sourceFile, targetFile, { overwrite: true });
-        console.log(`Moved ${sourceFile} to ${targetFile}`);
-      }
-
-      console.log('Files moved successfully.');
-    } else {
-      console.log('Source directory does not exist.');
-    }
-  } catch (err) {
-    console.error('Error moving files:', err);
-  }
+  // Remove the now empty 'browser' directory
+  fs.rmdirSync(sourceDir);
+} else {
+  console.error(`Source directory ${sourceDir} does not exist.`);
 }
-
-// Call the function to move files
-moveFiles();
